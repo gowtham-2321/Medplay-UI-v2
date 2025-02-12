8caf3887a478b522b002ade5a7dc0abc1fad98f3
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-02-12 15:35:46 +0530

    medplay music player files - old version

---
+++ static/js/searchBarAnimation.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let input = document.querySelector(".input");
+    let box = document.querySelector(".box-search");
+    let expanded = false;
+
+    box.addEventListener("mouseenter", function () {
+        if (!expanded) {
+            input.style.width = "350px";
+            input.style.borderRadius = "10px";
+            expanded = true;
+        }
+    });
+});
